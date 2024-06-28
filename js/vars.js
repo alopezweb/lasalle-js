c7c6f7559d86a1d1190f5b2a2e185776e31201f9
Alberto López <alopezrweb@gmail.com>
2024-06-28 21:23:59 +0200

    initial commit

---
+++ js/vars.js
@@ -0,0 +1,9 @@
+userName = "Yo";
+userSurname = "Lopez";
+
+console.log(userName);
+console.log(userSurname);
+console.log(userName, userSurname);
+console.log(userName + userSurname);
+console.log(userName + " " + userSurname);
+
