3cb26cbaa105b4dd78b16784381c413040173aa4
Alberto López <alopezrweb@gmail.com>
2024-06-29 09:38:43 +0200

    new var

--- js/vars.js
+++ js/vars.js
@@ -1,9 +1,12 @@
 userName = "Yo";
 userSurname = "Lopez";
+title = "Sr";
 
 console.log(userName);
 console.log(userSurname);
 console.log(userName, userSurname);
 console.log(userName + userSurname);
 console.log(userName + " " + userSurname);
+console.log(title + userName + " " + userSurname);
+
 
